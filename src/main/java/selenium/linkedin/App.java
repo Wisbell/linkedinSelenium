815592b238500161b44f68ee118a89fe5a97da55
Westley <westleyisbell@gmail.com>
2017-09-05 19:23:26 -0500

    Added selenium tutorial code to get started quickly

--- src/main/java/selenium/linkedin/App.java
+++ src/main/java/selenium/linkedin/App.java
@@ -1,13 +1,42 @@
+// Based on official Selenium tutorial
+// Creating a linkedin account using selenium
+
 package selenium.linkedin;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class App  {
+    public static void main(String[] args) {
+    	// Give machine access to gecko driver for firefox tests
+    	System.setProperty("webdriver.gecko.driver", "C:\\Users\\wookie\\Documents\\seleniumDriverExecs\\geckodriver.exe");
+    	
+    	// Set browser capabilities for firefox testing
+    	DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+    	
+    	// Necessary to run firefox test
+        capabilities.setCapability("marionette", true);
+        
+        // Create a new instance of the Firefox driver
+        // Notice that the remainder of the code relies on the interface, 
+        // not the implementation.
+        WebDriver driver = new FirefoxDriver();
+        
+        // Test to make sure selenium is loaded and works
+
+        // And now use this to visit Google
+        driver.get("http://www.google.com");
+        
+        // End of test
+        
+        // Plan flow of program here
+        
+        //Close the browser
+//        driver.quit();
     }
 }
